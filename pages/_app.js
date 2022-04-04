61adfa1b77e5d006e532a4e44a13224125a89b14
Rishabh Singh <rishabh@happymonk.co>
2022-04-04 10:10:08 +0530

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer";
 import "../styles/globals.css";
 import { createClient, Provider } from "urql";
+import Head from "next/head";
 
 const client = createClient({
   url: "https://api.ckdr.co.in/graphql",
@@ -13,6 +14,9 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Provider value={client}>
         <Component {...pageProps} />
         <Footer />
