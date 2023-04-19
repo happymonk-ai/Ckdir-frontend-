8c4d6fca671ac538d15c692f5c62cd7941b006d8
Vinayak <88454618+VinayakaHegade@users.noreply.github.com>
2023-04-19 23:09:14 +0530

    book demo page implementation (#19)
    * update book demo page
    
    * fix: submit button spacing
    
    * fix: fonts and back btn
    
    * add industry sector icons
    
    * fix: select industry list
    
    * fix: fonts and icon quality
    
    * fix: fonts height and icons quality

---
+++ components/Svg.jsx
@@ -0,0 +1,24 @@
+const MySVG = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.00041 9.01221C3.193 9.01221 0.54541 11.2865 0.54541 15.4161C0.54541 15.7386 0.806816 16 1.12935 16H14.8714C15.194 16 15.4554 15.7386 15.4554 15.4161C15.4554 11.2867 12.8078 9.01221 8.00041 9.01221ZM1.73454 14.8322C1.96422 11.7445 4.06932 10.1801 8.00041 10.1801C11.9315 10.1801 14.0366 11.7445 14.2665 14.8322H1.73454Z"
+        fill="#2E3034"
+        fillOpacity="0.4"
+        // stroke="#000"
+        // strokeWidth="2"
+        // strokeLinecap="round"
+        // strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MySVG;
