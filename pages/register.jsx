1b58c37e8e74e613aca6f6850f822cd9a3e10d58
amani-eric <104340923+amani-eric@users.noreply.github.com>
2023-05-02 08:23:58 +0200

    Dev (#25)
    * book demo page implementation (#19)
    
    * update book demo page
    
    * fix: submit button spacing
    
    * fix: fonts and back btn
    
    * add industry sector icons
    
    * fix: select industry list
    
    * fix: fonts and icon quality
    
    * fix: fonts height and icons quality
    
    * Add validation for book demo form (#20)
    
    * Update book demo page layout
    Fix spacing and font issues on the book demo page
    Add formik adn yup validations for book demo form
    
    * fix: fonts and back btn
    
    * fix: select industry list
    
    * fix: fonts and icon quality
    
    * fix: fonts height and icons quality
    
    * add formik and yup validations
    
    * fix: name sector and org validation
    
    * resolve conflicts
    
    * fix: build error
    
    * fix: height of page
    
    * fix(sendinblue): fix sendinblue (#24)
    
    ---------
    
    Co-authored-by: Vinayak <88454618+VinayakaHegade@users.noreply.github.com>

--- pages/register.jsx
+++ pages/register.jsx
@@ -2,10 +2,9 @@ import { useEffect, useState } from "react";
 // import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useFormik } from "formik";
-import * as Yup from "yup";
-
 import Image from "next/image";
 import Link from "next/link";
+import * as Yup from "yup";
 import logo from "../public/chokidr-logo.svg";
 import iMacImg from "../public/hp 1.png";
 import backIcon from "../public/icons/back-icon.svg";
@@ -88,8 +87,6 @@ const Register = () => {
 
 
   const handleSubmit = (data, setSubmitting) => {
-
-    console.log(data)
     setSubmitting(true);
     axios
       .post(
@@ -166,14 +163,13 @@ const Register = () => {
                     </div>
                   </div>
                   <div className={styles.label}>Enter your details here</div>
-                    {error && (
+                  {error && (
                     <span className={styles.errorMessage}>
                       {errorMessage} <br />
                     </span>
-                    )}
+                  )}
                   <div className={styles.fields}>
                     <form onSubmit={formik.handleSubmit}>
-                      {/* register your input into the hook by invoking the "register" function */}
                       <div className={styles.field}>
                         <div className={inputStyles.input}>
                           <span className={inputStyles.icon}>
@@ -366,7 +362,7 @@ const Register = () => {
                           />
                         </div>
                         {formik.touched.organisation &&
-                            formik.errors.organisation ? (
+                          formik.errors.organisation ? (
                           <p className={styles.errorMessage}>
                             {formik.errors.organisation}
                           </p>
@@ -386,18 +382,18 @@ const Register = () => {
                           />
                         </div>
                         {formik.touched.companySize &&
-                            formik.errors.companySize ? (
+                          formik.errors.companySize ? (
                           <p className={styles.errorMessage}>
                             {formik.errors.companySize}
                           </p>
                         ) : (
                           ""
                         )}
-                        </div>
+                      </div>
                       <div className={selectStyles.submitBtn}>
-                          <button type="submit" className={btnStyles.button} style={{
-                            pointerEvents: formik.isSubmitting ? "none" : "auto"
-                          }}>   {formik.isSubmitting ? 'Submitting...' : 'Submit'}</button>
+                        <button type="submit" className={btnStyles.button} style={{
+                          pointerEvents: formik.isSubmitting ? "none" : "auto"
+                        }}>   {formik.isSubmitting ? 'Submitting...' : 'Submit'}</button>
                       </div>
                     </form>
                   </div>
